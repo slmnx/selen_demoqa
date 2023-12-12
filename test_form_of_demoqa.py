36dd677cae01e303e680a5d22fbd0260efd52231
Xazri Suleymanov <slmnx94@gmail.com>
2023-12-12 21:00:37 +0300

    Try pass homework 5. Selene #1. Test not completed successfully. I need help

---
+++ test_form_of_demoqa.py
@@ -0,0 +1,34 @@
+from selene import browser, have
+import os
+def test_form_of_demoqa():
+    browser.open('https://demoqa.com/automation-practice-form')
+
+    browser.element('#firstName').type('Bebish')
+    browser.element('#lastName').type('Bebushov')
+    browser.element('#userEmail').type('beb.bobushov@mail.ru')
+    browser.element('label[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type('9091234567')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('[class="react-datepicker__month-select"]').click().type('April').click()
+    browser.element('[class="react-datepicker__year-select"]').click().type('1994').click()
+    browser.element('[class="react-datepicker__day react-datepicker__day--028"]').click()
+    browser.element('#subjectsInput').click().type('history').press_enter()
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('#currentAddress').type('Moscow leskova')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('Noida').press_enter()
+    browser.element('#submit').click()
+
+    #checking
+
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element('.modal-body').should(have.text('Bebish Bebushov'))
+    browser.element('.modal-body').should(have.text('beb.bobushov@mail.ru'))
+    browser.element('.modal-body').should(have.text('Male'))
+    browser.element('.modal-body').should(have.text('9091234567'))
+    browser.element('.modal-body').should(have.text('28 April,1994'))
+    browser.element('.modal-body').should(have.text('history'))
+    browser.element('.modal-body').should(have.text('Sports'))
+    browser.element('.modal-body').should(have.text('Moscow leskova'))
+    browser.element('.modal-body').should(have.text('NCR Noida'))
+
